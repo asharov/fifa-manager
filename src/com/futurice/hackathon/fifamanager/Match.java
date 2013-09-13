ecd806a4ac8f553f6d7388d2165d07586cad761d
Martin Richter <martin.richter@futurice.com>
2013-09-13 20:14:14 +0200

    Add match and tournament models

---
+++ src/com/futurice/hackathon/fifamanager/Match.java
@@ -0,0 +1,5 @@
+package com.futurice.hackathon.fifamanager;
+
+public class Match {
+
+}
---
+++ src/com/futurice/hackathon/fifamanager/Tournament.java
@@ -0,0 +1,10 @@
+package com.futurice.hackathon.fifamanager;
+
+import java.util.ArrayList;
+
+public class Tournament {
+	
+	public ArrayList<Team> teams;
+	public ArrayList<Match> matches;
+
+}
